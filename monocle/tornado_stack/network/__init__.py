7290a1f2ef33c92045f708415aa24ff19132298e
Greg Hazel <ghazel@gmail.com>
2016-07-06 00:54:08 -0700

    read_some for modern Tornado

--- monocle/tornado_stack/network/__init__.py
+++ monocle/tornado_stack/network/__init__.py
@@ -21,38 +21,16 @@ from monocle.util import monkeypatch
 
 # monkeypatch IOStream to provide a reactive read
 @monkeypatch(IOStream)
-def __init__(orig_method, self, *a, **k):
-    orig_method(self, *a, **k)
-    self._read_some = None
-
-
-@monkeypatch(IOStream)
-def _read_from_buffer(orig_method, self, *a, **k):
-    if self._read_some is not None:
-        if self._read_buffer_size > 0:
-            callback = self._read_callback
-            self._read_callback = None
-            self._read_some = None
-            self._run_callback(callback, self._consume(self._read_buffer_size))
-            return True
-    else:
-        return orig_method(self, *a, **k)
-
-
-@monkeypatch(IOStream)
 def read_some(orig_method, self, callback):
     """Call callback when we read some bytes."""
     from tornado import stack_context
-    assert not self._read_callback, "Already reading"
-    self._read_some = True
-    self._read_callback = stack_context.wrap(callback)
-    while True:
-        if self._read_from_buffer():
-            return
-        self._check_closed()
-        if self._read_to_buffer() == 0:
-            break
-    self._add_io_state(self.io_loop.READ)
+    cb = stack_context.wrap(callback)
+    def streaming_callback(data):
+        self._read_future = None
+        self._read_until_close = False
+        self._streaming_callback = None
+        cb(data)
+    self.read_until_close(None, streaming_callback)
 
 
 class _Connection(object):
